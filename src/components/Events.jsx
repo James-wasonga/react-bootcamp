9a39b4c3941c0c3975d5f75f6059c889ca1e50ae
James-wasonga <jameswasonga22@gmail.com>
2024-02-28 22:47:22 +0300

    function update

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -5,10 +5,13 @@ const Events = () => {
     function handleClick(){
         console.log("Hello you clicked me well");
     }
-
+function handle (){
+  console.log("welcome");
+}
   return (
     <>
     <button className='event' onClick={handleClick}>Click</button>
+    <button onClick={handle}>Check click</button>
     </>
   )
 }
